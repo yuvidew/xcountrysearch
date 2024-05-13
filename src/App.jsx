f9dae89dec6e6019fac4228766b0bd4c151061ac
Yuvi dew <127539111+yuvidew@users.noreply.github.com>
2024-05-13 17:08:57 +0530

    update file

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ const App = () => {
   const [country , setCountry] = useState([])
   const [search , setSearch] = useState('')
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .then(data => setCountry(data))
-    .catch(e => console.error(e))
-  } , [])
+  const searchCountry = async () => {
+    try {
+      const data = await fetch("https://restcountries.com/v3.1/all")
+      const dataJson = await data.json()
+      setCountry(dataJson)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
+  useEffect(() => {
+    searchCountry()
+  } , [search])
+
+  
   const filteredData = country.filter(ele => ele.name.common.toLowerCase().includes(search.toLowerCase()))
 
   return (
@@ -30,7 +38,11 @@ const App = () => {
               key={ele.name.common}
               className='flex items-center justify-center flex-col gap-y-2 mt-[1rem]'
             >
-                <img className='h-[70%] w-[80%] object-contain' src={ele.flags.svg}  alt={ele.flags.alt} />
+                <img 
+                  className='h-[70%] w-[80%] object-contain' 
+                  src={ele.flags.svg}  
+                  alt={ele.flags.alt} 
+                />
                 <p>{ele.name.common}</p>
             </div>
           ))
